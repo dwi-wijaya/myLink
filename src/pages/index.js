9e234a1c2ee4b97c4f129351127dda8043a6a481
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-04 22:41:01 +0700

    fix: index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,4 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import LoginForm from "@/components/form/LoginForm";
 import { useUser } from "@/context/user";
 import Container from "@/components/layout/Container";
 const inter = Inter({ subsets: ["latin"] });
