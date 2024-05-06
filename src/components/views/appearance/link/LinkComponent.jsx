d5e5d95b0eb81d9efefcf0a37a455cab495910cf
Dwi Wjiaya <dev.dwiwijaya@gmail.com>
2024-05-06 08:52:03 +0700

    feat: edit link url

--- src/components/views/appearance/link/LinkComponent.jsx
+++ src/components/views/appearance/link/LinkComponent.jsx
@@ -1,27 +1,41 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
+import React, { useEffect, useRef, useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
-const LinkComponent = ({ id, title, url, order, handleDelete, setLinks, links }) => {
-    const { attributes, listeners, setNodeRef, transform, transition } = useSortable({ id });
+const LinkComponent = ({
+    id,
+    title,
+    url,
+    order,
+    handleDelete,
+    setLinks,
+    links,
+}) => {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+        useSortable({ id });
 
     const style = {
         transition,
         transform: CSS.Transform.toString(transform),
     };
 
-    const [linkTitle, setLinktitle] = useState(title);
-    const [onUpdate, setOnupdate] = useState(false)
+    const [linkValue, setlinkValue] = useState({
+        title: title,
+        url: url,
+    });
+    const [onUpdate, setOnupdate] = useState({
+        title: false,
+        url: false,
+    });
 
     const handleChange = (e) => {
-        setLinktitle(e.target.value)
-        const id = e.target.id
+        const id = e.target.id;
+        setlinkValue({ ...linkValue, [e.target.name]: e.target.value });
         // Find the index of the object with the given ID
-        handleUpdate(id, e.target.value, e.target.name)
-    }
+        handleUpdate(id, e.target.value, e.target.name);
+    };
 
     const inputRef = useRef(null);
-
 
     useEffect(() => {
         const handleClickOutside = (event) => {
@@ -29,7 +43,6 @@ const LinkComponent = ({ id, title, url, order, handleDelete, setLinks, links })
                 setOnupdate(false);
             }
         };
-
 
         document.addEventListener("mousedown", handleClickOutside);
         return () => {
@@ -39,9 +52,7 @@ const LinkComponent = ({ id, title, url, order, handleDelete, setLinks, links })
 
     const handleUpdate = (id, value, type) => {
         console.log(links);
-        const index = links.findIndex(link =>
-            link.id == id)
-        console.log(index);
+        const index = links.findIndex((link) => link.id == id);
         // // If the object is not found, return without updating
         if (index === -1) return;
 
@@ -49,28 +60,63 @@ const LinkComponent = ({ id, title, url, order, handleDelete, setLinks, links })
         // const newArray = [...yourArray];
         const updatedLinks = [...links];
         // // Update the specific object within the copied array
-        updatedLinks[index] = { ...updatedLinks[index], title: value };
+        updatedLinks[index] = { ...updatedLinks[index], [type]: value };
 
         // // Set the state with the updated array
         setLinks(updatedLinks);
-
-    }
+    };
 
     return (
-        <div ref={setNodeRef} style={style} {...attributes} className="bg-container border border-stroke px-3 py-2 rounded-lg  touch-none cursor-default">
+        <div
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            className="bg-container border border-stroke px-3 py-2 rounded-lg  touch-none cursor-default"
+        >
             <div className="flex justify-between items-center">
                 <div className="flex items-center gap-3">
-                    <button className='btn !bg-background !border-stroke' {...listeners}><i className="bx bx-grid-alt"></i></button>
+                    <button className="btn !bg-background !border-stroke" {...listeners}>
+                        <i className="bx bx-grid-alt"></i>
+                    </button>
                     <div className="flex flex-col">
-
                         <p className="flex items-center gap-3 mb-0">
-                            {onUpdate && <input ref={inputRef} id={id} name='title' onChange={handleChange} type="text" className='w-fit p-0 outline-offset-2 outline-transparent border-none box-shadow-none' value={linkTitle} />}
-                            {!onUpdate && <p>{linkTitle}</p>}
-                            {!onUpdate && <i onClick={() => setOnupdate(true)} className="bx bx-pencil"></i>}
+                            {onUpdate.title && (
+                                <input
+                                    ref={inputRef}
+                                    id={id}
+                                    name="title"
+                                    onChange={handleChange}
+                                    type="text"
+                                    className="w-fit bg-transparent p-0 outline-offset-2 outline-transparent border-none box-shadow-none"
+                                    value={linkValue.title}
+                                />
+                            )}
+                            {!onUpdate.title && <p>{linkValue.title}</p>}
+                            {!onUpdate.title && (
+                                <i
+                                    onClick={() => setOnupdate({title:true})}
+                                    className="bx bx-pencil"
+                                ></i>
+                            )}
                         </p>
 
-                        <p className="flex items-center gap-3 text-subtext">
-                            {url} {!onUpdate && <i onClick={() => setOnupdate(true)} className="bx bx-pencil"></i>}
+                        <p className="flex items-center gap-3 mb-0">
+
+                            {onUpdate.url && (
+                                <input
+                                    ref={inputRef}
+                                    id={id}
+                                    name="url"
+                                    onChange={handleChange}
+                                    type="text"
+                                    className="w-fit.url bg-transparent p-0 outline-offset-2 outline-transparent border-none box-shadow-none"
+                                    value={linkValue.url}
+                                />
+                            )}
+                            {!onUpdate.url && <p>{linkValue.url}</p>}
+                            {!onUpdate.url && (
+                                <i onClick={() => setOnupdate({ url: true })} className="bx bx-pencil"></i>
+                            )}
                         </p>
                     </div>
                 </div>
