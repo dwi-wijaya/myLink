7156c0af6e5bfb1091556c208ec67a4e836aa7ef
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-09 16:38:30 +0700

    config: firebase on signint with google or sign up if new user create firestore document

--- src/services/firebase/config.js
+++ src/services/firebase/config.js
@@ -4,11 +4,15 @@ import { getStorage } from 'firebase/storage'
 import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  getAdditionalUserInfo,
   getAuth,
   signInWithEmailAndPassword,
   signInWithPopup,
   signOut
 } from "firebase/auth";
+import { getFirestore, doc, setDoc } from "firebase/firestore";
+
+import { v4 } from "uuid";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -33,20 +37,36 @@ export const SignIn = async (email, password) => {
   await signInWithEmailAndPassword(FirebaseAuth, email, password)
 }
 export const CreateUser = async (email, password) => {
-  await createUserWithEmailAndPassword(FirebaseAuth, email, password)
+  const result = await createUserWithEmailAndPassword(FirebaseAuth, email, password)
+  const { isNewUser } = getAdditionalUserInfo(result)
+  if (isNewUser) {
+    const db = getFirestore(firebase_app)
+    const data = {
+      uid: result.user.uid,
+      email: result.user.email,
+      timestamp: Date.now()
+    }
+    await setDoc(doc(db, 'links', v4()), data, {
+      merge: true,
+    });
+  }
 }
 
 export const GoogleSignIn = async () => {
   const provider = new GoogleAuthProvider();
 
-  const result = await signInWithPopup(getAuth(), provider);
-
-  // If the account doesn't exist, create it
-  console.log(result.user);
-  if (result.additionalUserInfo.isNewUser) {
-    // const { email } = result.user;
-    // Use the email as the password for simplicity
-    // await createUserWithEmailAndPassword(getAuth(), email, email);
+  const result = await signInWithPopup(getAuth(), provider)
+  const { isNewUser } = getAdditionalUserInfo(result)
+  if (isNewUser) {
+    const db = getFirestore(firebase_app)
+    const data = {
+      uid: result.user.uid,
+      email: result.user.email,
+      timestamp: Date.now()
+    }
+    await setDoc(doc(db, 'links', v4()), data, {
+      merge: true,
+    });
   }
 }
 
