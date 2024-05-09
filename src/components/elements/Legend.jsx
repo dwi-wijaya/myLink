0736510f09e09b0acc5f92b6f8a30394000777bf
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-09 13:21:14 +0700

    style: legend add margin bottom

--- src/components/elements/Legend.jsx
+++ src/components/elements/Legend.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Legend = ({ title, className, children }) => {
     return (
-        <div className={`relative border border-slate-500 border-slate-500 p-4 py-5 rounded-lg ${className}`}>
-            <div className="text-lg absolute -top-4 ml-2 bg-background px-2 font-semibold tracking-wide">{title}</div>
+        <div className={`relative border mb-6 border-slate-500 border-slate-500 p-4 py-5 rounded-lg ${className}`}>
+            <div className="text-lg absolute -top-4 bg-background px-2 font-semibold tracking-wide">{title}</div>
             {children}
         </div>
     )
