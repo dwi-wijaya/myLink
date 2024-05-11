9b5e5b2e23741e0dbed91326a0f32ad5bda3f5c1
Dwi Wijaya <dev.dwiwijaya@gmail.com>
2024-05-11 13:49:12 +0700

    feat: view profile page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,15 @@ import Container from "@/components/layout/Container";
 import AppearanceSetting from "@/components/views/appearance/AppearanceSetting";
 import WithProtected from "@/hoc/withProtected";
 const inter = Inter({ subsets: ["latin"] });
+import SignoutToggle from "@/components/toggles/SignoutToggle";
+import ThemeToggle from "@/components/toggles/ThemeToggle";
 
-const  Home = () => {
+const Home = () => {
   const user = useUser();
   return (
     <Container>
+      <ThemeToggle />
+      <SignoutToggle />
       <AppearanceSetting />
     </Container>
   );
